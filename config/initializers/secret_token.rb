154bf6ada1f7dc3c4f52fbd76a08c94c804f2db0
jibasig <jibasig@jibsig.jibsig.loca>
2011-12-12 18:10:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsbox::Application.config.secret_token = '51b37f3acce0564ad1f64b0c2e4989b6382a95f351c2ba530a013b3466600deb46ea502e4801d8b4d5f8091f4eced0edcfbd8e4dfe3580f2e63939e2247db2b7'
